f91825ab580145352ef25e82f13d3b8c4c645e2c
Oluwayomi Adekanye <kanyeunited@gmail.com>
2025-02-27 05:38:19 +0100

    edited new feature

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-God is good
+Gods is good
 all the time
+He has been marvelous in my life
+I love Him so much
+God im sorry for all my transgressions
+forgive me for all my sins
+I accept Jesus as my personal Lord and Saviour
+Come into my life and sup with me
+Wash me clean
+Purge me whiter than snow
+write my name in the book of life
+I am save by faith
+not by merit but by God's grace
 
 
 
